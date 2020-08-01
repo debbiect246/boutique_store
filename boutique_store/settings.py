e94d8fc4092afe2ff06b559f82bc3c53ce7213a7
Debbie Thompson <deb246@btinternet.com>
2020-08-01 23:24:42 +0100

    errors in settings corrected

--- boutique_store/settings.py
+++ boutique_store/settings.py
@@ -79,15 +79,16 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 
-SITE_ID= 1
+]
 
-WSGI_APPLICATION= 'boutique_store.wsgi.application'
+WSGI_APPLICATION = 'boutique_store.wsgi.application'
 
+SITE_ID = 1
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES= {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -98,7 +99,7 @@ DATABASES= {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS= [
+AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -117,18 +118,18 @@ AUTH_PASSWORD_VALIDATORS= [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE= 'en-us'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE= 'UTC'
+TIME_ZONE = 'UTC'
 
-USE_I18N= True
+USE_I18N = True
 
-USE_L10N= True
+USE_L10N = True
 
-USE_TZ= True
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL= '/static/'
+STATIC_URL = '/static/'
